75c0c31a35284a248b70d4b3b5018cb323a2ef52
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2022-08-25 14:10:58 +0200

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <main className="app-main flex">
         <Hero />
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="app-footer">
+      <p>
+        <span className="text-shadow">&copy; 2022 </span>
+        <a
+          href="https://github.com/dragi-ns/shopping-cart"
+          target="_blank"
+          rel="noopener noreferrer">
+          dragi-ns
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
